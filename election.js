452b9d682cc2f33eeab20407c62ac56d1469b7dd
Justin Cheung <justinwkcheung@github.com>
2017-02-17 13:34:17 -0500

    Loop over json api to display name and votes in list

--- election.js
+++ election.js
@@ -1,5 +1,15 @@
 $(document).ready(function() {
 
-  // Imagination!
+  $.ajax({
+    url: 'https://bb-election-api.herokuapp.com/',
+    method: 'GET',
+    dataType: 'json'
+  }).done(function(data){
 
+    for (var i = 0; i < data.candidates.length; i++) {
+      console.log(data.candidates);
+      $('<li>').html(data.candidates[i].name + "  Votes: " + data.candidates[i].votes).appendTo('#candidateList');
+
+    };
+  });
 });
